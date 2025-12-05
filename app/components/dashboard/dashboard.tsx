fce27a73ac9977addb38e1b1636cbb8dce52b74b
OfOluo <aboubakari.camarargy@gmail.com>
2025-12-05 16:31:04 +0100

    feat: Polished navbar components

--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -1,19 +1,27 @@
+'use client'
+
 import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function Dashboard() {
+  
+  const [visible, IsVisible ] = useState(null);
 
   return (
 
     <div className="pt-6">
 
       <div className="flex items-center space-x-5 relative">
-        <button className="text-gray-200">Board View</button>
-        <button className="text-gray-200">Add view</button>
+        <button
+         className="text-gray-500"
+        
+        >
+          Board View</button>
+        <button className="text-gray-500">Add view</button>
 
           <div className="absolute right-0 ml-2 flex items-center space-x-4">
 
             <span className="text-gray-400 text-sm font-bold cursor-pointer">Filter</span>
-            <span className="text-gray-400 text-sm font-bold cursor-pointer">Sort</span>
             
           <button className="flex items-center cursor-pointer rounded-full bg-blue-500 text-white px-4 py-2 ml-3">  New Task <Plus size={20} className="ml-2" /></button>
          
@@ -22,19 +30,21 @@ export default function Dashboard() {
 
       <div className="border border-gray-300 mt-4 w-full rounded-full "></div>
 
-      <div className="flex justify-center mt-4">
+      <div className="flex cols-span-3 space-x-4 t-4">
         
-        <div className="kanban-col relative w-full max-w-md">
+        <div className="kanban-col relative w-full max-w-md mt-4">
 
           <div className="flex justify-between items-center w-full mb-4">
 
             <div>
-             <span className="text-sm font-bold text-gray-200">To do(5)</span>
+             <span className="text-sm font-bold text-gray-200">To do(0)</span>
             </div>
 
             <div>
-              <button className="flex items-center space-x-2 text-gray-200 hover:text-gray-100">
-                <Plus size={20} /> 
+              <button className="flex items-center space-x-2 text-gray-200 cursor-pointer hover:text-gray-100">
+                <Plus size={23}
+                className="bg-gray-500 py-1 rounded-full"
+                /> 
                 <span>Add New Tasks</span>
               </button>
             </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,12 +5,17 @@ export default function Navbar() {
 
   return (
     <div className="w-full h-16 flex items-center justify-end px-6 border-b border-gray-700 bg-[#1f1f1f]">
-      <Search size={20} className="btn-gray" />
-      <Bell size={20} className="btn-gray" />
-      <div className="flex space-x-3 cursor-pointer rounded-sm hover:bg-gray-700 p-2">
+        
+        <div className="space-x-3 mr-3 flex">
+        <Search size={20} className="btn-gray" />
+        <Bell size={20} className="btn-gray" />
+        </div>
+      
+      <div className="flex space-x-2 cursor-pointer rounded-sm hover:bg-gray-700 p-2">
         <Calendar size={20} className="btn-gray" />
         <span className="text-gray-300 text-sm font-bold">04 Nov 2025</span>
       </div>
+
       <div className="w-8 h-8 rounded-full bg-gray-400"></div>
     </div>
   );
