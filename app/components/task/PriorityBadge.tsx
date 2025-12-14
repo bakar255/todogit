36fb65a556bfc55b6a46d53f446814ea16c41632
OfOluo <aboubakari.camarargy@gmail.com>
2025-12-14 15:43:17 +0100

    refactor: change props details in Priority badge component

--- app/components/task/PriorityBadge.tsx
+++ app/components/task/PriorityBadge.tsx
@@ -7,15 +7,15 @@ interface PriorityProps {
 
 const priorityMap: Record<Priority, { class: string; label: string }> = {
     low: { 
-      class: 'text-xs px-2 py-1 border border-blue-200 rounded bg-blue-100', 
+      class: 'text-xs px-2 py-0.5 text-white border border-blue-200 rounded bg-blue-100', 
       label: 'Low' 
     },
     medium: { 
-      class: 'text-xs px-2 py-1 border border-amber-200 rounded bg-orange-200 ', 
+      class: 'text-xs text-white px-2 py-0.5 rounded bg-orange-500', 
       label: 'Medium' 
     },
     high: { 
-      class: 'text-xs px-2 py-1 border border-red-200 rounded bg-red-300 ', 
+      class: 'text-xs text-white px-2 py-0.5 rounded border-1 bg-red-400 ', 
       label: 'High' 
     },
   };
